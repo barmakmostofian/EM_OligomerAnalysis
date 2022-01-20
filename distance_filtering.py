ca21f53c4ac8378b6ec62c5b6ad43113fbbcd489
barmakmostofian <barmakmostofian@users.noreply.github.com>
2022-01-20 19:21:50 +0100

    Create distance_filtering.py

---
+++ distance_filtering.py
@@ -0,0 +1,43 @@
+import sys
+import subprocess
+import numpy as np
+from numpy import (array, dot, arccos, clip)
+from numpy.linalg import norm
+from micrograph_operations import assign_oligomers
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+FileIn = sys.argv[1]
+MinDist = float(sys.argv[2])  # The clearance distance for oligomers
+
+npzfile  = np.load(FileIn, allow_pickle=True)
+DataName = FileIn.split("../")[1].split(".npz")[0]
+All_Coors = npzfile['arr_0'].tolist()
+Oligomer_Coors_ByLength  = npzfile['arr_1'].tolist()
+Oligomer_Scores_ByLength = npzfile['arr_2'].tolist()
+
+print("\nThere are a total of", len(All_Coors), "beads\n")
+
+
+All_Oligomers = []
+All_Oligomer_Coors = []
+for oligolen in Oligomer_Coors_ByLength.keys() :
+   for oligomer in Oligomer_Coors_ByLength[oligolen] :
+      All_Oligomers.append(oligomer)
+      for bead in oligomer :
+         All_Oligomer_Coors.append(bead)
+
+print("There are", len(All_Oligomer_Coors), "beads assigned to", len(All_Oligomers), "oligomers\n")
+
+[Singleton_Coors,
+Cleared_Singleton_Coors,
+UnCleared_Singleton_Coors,
+Cleared_Oligomer_Coors_ByLength,
+UnCleared_Oligomer_Coors_ByLength,
+Blocked_Oligomer_Coors_ByLength,
+Possible_Oligomer_Coors_ByLength,
+Locked_Oligomer_Coors_ByLength,
+Excluded_Coors]  =  assign_oligomers(All_Coors, Oligomer_Coors_ByLength, MinDist)
+
